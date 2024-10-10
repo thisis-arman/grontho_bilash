0540c99a071337cff1d06c0ccb93a907ca571812
Arman Hossen <armanhossen1549@gmail.com>
2024-10-10 23:02:01 +0600

    add product form

--- gb-frontend/src/pages/dashboard/adminDashboard/AdminDashboard.tsx
+++ gb-frontend/src/pages/dashboard/adminDashboard/AdminDashboard.tsx
@@ -4,6 +4,7 @@ const AdminDashboard = () => {
     return (
         <div>
             Admin Dashboard Home
+            <img src="https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*P0S-QIRUbsUAAAAAAAAAAABkARQnAQ" alt="" />
         </div>
     );
 };
