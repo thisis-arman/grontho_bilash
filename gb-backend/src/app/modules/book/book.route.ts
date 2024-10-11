f42b4106e61dc9f81cfccdc95ac94f2ab7243b64
Arman Hossen <armanhossen1549@gmail.com>
2024-10-11 20:15:03 +0600

    image uploader

--- gb-backend/src/app/modules/book/book.route.ts
+++ gb-backend/src/app/modules/book/book.route.ts
@@ -1,11 +1,38 @@
+import fs  from 'fs';
 import { zodValidationSchema } from "./book.validation";
-import express from "express";
+import express, { Request, Response } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { bookController } from "./book.controllers";
+import { imageUploader, upload } from "../../utils/imageUploader";
 
 const router = express.Router();
 
 router.get("/", bookController.getBooks);
+router.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    const imageName = req.file?.originalname;
+    const path = req.file?.path;
+
+    if (!imageName || !path) {
+      return res.status(400).json({ error: "Image file is required." });
+    }
+
+    // Upload image to Cloudinary
+    const result = await imageUploader(imageName, path);
+
+    // Delete the file after uploading to Cloudinary
+    fs.unlink(path, (err) => {
+      if (err) console.error("Failed to delete local file:", err);
+    });
+
+    // Send the Cloudinary URL as a response to the frontend
+    return res.status(200).json({ url: result?.secure_url });
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    return res.status(500).json({ error: "Failed to upload image" });
+  }
+});
+
 router.post(
   "/create-book",
   validateRequest(zodValidationSchema.createBookSchema),
