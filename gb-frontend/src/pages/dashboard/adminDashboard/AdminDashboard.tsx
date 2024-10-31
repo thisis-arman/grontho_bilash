b53e4538925284ea7aaff09384e155d8dbb67eb8
Arman Hossen <armanhossen1549@gmail.com>
2024-11-01 00:00:04 +0600

    updated

--- gb-frontend/src/pages/dashboard/adminDashboard/AdminDashboard.tsx
+++ gb-frontend/src/pages/dashboard/adminDashboard/AdminDashboard.tsx
@@ -1,10 +1,12 @@
+import Stats from "../../../components/ui/dashboardComponents/PieChart";
 
 
 const AdminDashboard = () => {
     return (
         <div>
             Admin Dashboard Home
-            <img src="https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*P0S-QIRUbsUAAAAAAAAAAABkARQnAQ" alt="" />
+            {/* <img src="https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*P0S-QIRUbsUAAAAAAAAAAABkARQnAQ" alt="" /> */}
+            <Stats/>
         </div>
     );
 };
