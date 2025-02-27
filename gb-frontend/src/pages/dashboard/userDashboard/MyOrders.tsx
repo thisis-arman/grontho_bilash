0dca3c62d0006cf38bd79b46b3fac48e6cb6daad
Arman Hossen <armanhossen1549@gmail.com>
2025-02-27 15:34:47 +0600

    Update MyOrders.tsx

--- gb-frontend/src/pages/dashboard/userDashboard/MyOrders.tsx
+++ gb-frontend/src/pages/dashboard/userDashboard/MyOrders.tsx
@@ -1,6 +1,17 @@
+import { useGetOrderByUserIdQuery } from "../../../redux/features/order/orderApi";
+import { selectCurrentUser, TUser } from "../../../redux/features/auth/authSlice";
 
-
+import { useAppSelector } from "../../../redux/hooks";
+// import { useGetBooksByEmailQuery } from "../../../redux/features/order/orderApi";
 const MyOrders = () => {
+
+        // const { data, isLoading } = useGetBooksQuery(undefined);
+        const { id } = useAppSelector(selectCurrentUser) as TUser;
+
+        console.log({id});
+        console.log(id);
+        const { data, isLoading } = useGetOrderByUserIdQuery(id);
+        console.log({ data });
     return (
         <div>
             my product that being sold
