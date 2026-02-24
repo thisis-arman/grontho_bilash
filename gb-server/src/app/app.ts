f14d6eb8cf7f3aeb1a582388d9548f6403d6bb07
Arman Hossen <armanhossen1549@gmail.com>
2026-02-24 15:00:11 +0600

    cors updated

--- gb-server/src/app/app.ts
+++ gb-server/src/app/app.ts
@@ -14,7 +14,7 @@ app.use(cookieParser());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  // "https://your-production-domain.com", // Add your Vercel URL here later
+  "https://gronthobilash.vercel.app", // Add your Vercel URL here later
 ];
 
 app.use(
