99aad8bea21c6005f4452963b56943a802ef2a93
Arman Hossen <armanhossen1549@gmail.com>
2026-02-24 14:26:56 +0600

    vercel file added

--- gb-client/src/shared/Navbar.tsx
+++ gb-client/src/shared/Navbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { logout, selectCurrentUser, TUser } from "../redux/features/auth/authSlice";
