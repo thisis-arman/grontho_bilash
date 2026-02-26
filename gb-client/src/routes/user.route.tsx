998a85641c61d48e02543d6bd6264f373cf56fcc
Arman Hossen <armanhossen1549@gmail.com>
2026-02-27 03:38:08 +0600

    icons updated

--- gb-client/src/routes/user.route.tsx
+++ gb-client/src/routes/user.route.tsx
@@ -1,4 +1,11 @@
-import { Book, LayoutDashboard, PlusSquare } from "lucide-react";
+import { 
+  LayoutDashboard, 
+  PlusSquare, 
+  BookOpen, 
+  ShoppingBag, 
+  History as HistoryIcon, 
+  UserCircle 
+} from "lucide-react";
 import AddProduct from "../pages/dashboard/userDashboard/AddProduct";
 import History from "../pages/dashboard/userDashboard/History";
 import MyOrders from "../pages/dashboard/userDashboard/MyOrders";
@@ -7,41 +14,41 @@ import UserDashboard from "../pages/dashboard/userDashboard/UserDashboard";
 import UserProfile from "../pages/dashboard/userDashboard/UserProfile";
 
 
-
 export const userPaths = [
-
     {
         name: "Dashboard",
         path: "dashboard",
-        icon:<LayoutDashboard/>,
+        icon: <LayoutDashboard size={18} />,
         element: <UserDashboard />,
     },
     {
         name: "Add Product",
         path: "add-product",
-        icon:<PlusSquare/>,
+        icon: <PlusSquare size={18} />,
         element: <AddProduct />,
     },
     {
         name: "My Products",
         path: "my-Products",
-        icon:<Book/>,
+        icon: <BookOpen size={18} />, // BookOpen looks slightly more modern than Book
         element: <MyProducts />,
     },
     {
         name: "My Orders",
         path: "my-orders",
+        icon: <ShoppingBag size={18} />,
         element: <MyOrders />,
     },
     {
         name: "History",
         path: "history",
+        icon: <HistoryIcon size={18} />,
         element: <History />,
     },
     {
         name: "Profile",
         path: "user-profile",
+        icon: <UserCircle size={18} />,
         element: <UserProfile />,
     },
-  
-]
+];
