53577871a50f13ee2b80aea6b0c1a334e43339aa
Arman Hossen <armanhossen1549@gmail.com>
2026-06-30 23:33:19 +0600

    feat: implement MainLayout with ScrollToTop utility for page transitions

--- gb-client/src/components/layouts/MainLayout.tsx
+++ gb-client/src/components/layouts/MainLayout.tsx
@@ -3,6 +3,7 @@ import Footer from "../../shared/Footer";
 import Navbar from "../../shared/Navbar";
 import { useAppSelector } from "../../redux/hooks";
 import { selectCurrentUser } from "../../redux/features/auth/authSlice";
+import ScrollToTop from "../../utils/ScrollToTop";
 
 const MainLayout = () => {
     const user = useAppSelector(selectCurrentUser);
@@ -10,7 +11,9 @@ const MainLayout = () => {
     return (
         <div>
             <Navbar />
+            <ScrollToTop />
             <div className={`pt-16 ${user ? "pb-16 md:pb-0" : ""}`}>
+
                 <Outlet />
             </div>
             <Footer />
